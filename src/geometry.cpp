39acd14c10256f32589968baae4b484104f13615
ianmwendwaaa <williamsian845@gmail.com>
2026-01-23 23:21:24 +0300

    Fix: Quadratic Refinement

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -43,12 +43,33 @@ std::pair<double, double> Geometry::QuadraticRoots(const std::string& query) {
     // Find the roots applying the formula: (-b+-(b2 - 4ac).sqrt)/2a
 
     // Check if the square root of discriminant is positive, negative or equal to zero
-    double discriminantSqrt = std::sqrt(std::pow(this->b, 2) - (4 * this->a * this->c));
-    double x1 = (-this->b + std::sqrt(std::pow(this->b, 2) - (4 * this->a * this->c)))/(2 * this->a);
-    double x2 = (-this->b - std::sqrt(std::pow(this->b, 2) - (4 * this->a * this->c)))/(2 * this->a);
+    const double discriminant = std::pow(this->b, 2) - (4 * this->a * this->c);
 
-    std::cout << "x1: " << x1 << " x2: " << x2 << std::endl;
-    return std::make_pair(x1, x2);
+    const double two_a = 2 * this->a;
+
+    // If it is positive, and greater than 0, it has two real and distinct roots
+    if (discriminant > 0) {
+        const double discriminantSqrt = std::sqrt(discriminant);
+        const double x1 = (-this->b + discriminantSqrt)/two_a;
+        const double x2 = (-this->b - discriminantSqrt)/two_a;
+        std::cout << "x1: " << x1 << " x2: " << x2 << std::endl;
+    }
+
+    // If it is negative, it has no real roots, or two complex roots
+    if (discriminant < 0) {
+        // Express the result in its simplest form, should b be a multiple of 2a
+        if (std::fmod(this->b, two_a) == 0.0) {
+            std::cout << "x1: "<< -this->b / two_a << " + sqrt("<<discriminant<< ")"<<
+                " or x1: " << -this->b / two_a <<(discriminant > 0.0 ? "+":"-") << std::abs(discriminant) <<"i\n";
+            std::cout << "x2: "<< -this->b / two_a << " + √"<<discriminant<<
+                " or x2: " << -this->b / two_a <<(discriminant > 0.0 ? "+":"-") << std::abs(discriminant) <<"i\n";
+        }else {
+            std::cout << "x1: " <<-this->b << "+ √"<<discriminant << ")/" << two_a<<"\n";
+            std::cout << "x2: " <<-this->b << "- √"<<discriminant << ")/" << two_a<<"\n";
+        }
+    }
+
+    return std::make_pair(1.0, 1.0);
 }
 
 Geometry::~Geometry() = default;
