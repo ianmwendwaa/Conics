7cd08634e3615ad46fc5c30c07c14f2feeda8e13
ianmwendwaaa <williamsian845@gmail.com>
2026-01-24 16:32:28 +0300

    Quadratics refinement

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -7,6 +7,38 @@
 
 Geometry::Geometry() = default;
 
+inline auto findSimplestSurd = [](const int& d) {
+    // First check if the number is a prime number. If so, leave it untouched
+    bool is_prime{};
+    double result = 0;
+
+    for (size_t i = 2; i <= std::sqrt(d); i++) {
+        if (std::fmod(d, i) != 0) {
+            is_prime = true;
+            break;
+        }
+    }
+    if (is_prime) return d;
+
+    // To find the surd, find the greatest perfect square. It should be less than the provided number
+    double gps = 0;// greatest perfect square
+
+    // Vector to store the values found
+    std::vector<int> surdValues;
+    for (size_t i = d; i > 0; i--) {
+        if (std::sqrt(i) == d) {
+            surdValues.emplace_back(i);
+        }
+    }
+};
+inline bool isPerfectSquare(long long d) {
+    if (d <= 0) return false;// Cannot find square root of 0 or negative numbers
+
+    if (std::sqrt(d) != d) return false;// Square root is not equal to provided number
+
+    return true;// If both are false, d is a perfect square
+
+}
 std::pair<double, double> Geometry::QuadraticRoots(const std::string& query) {
     std::string cleanQuery = query;
     cleanQuery.erase(std::ranges::remove(cleanQuery, ' ').begin(), cleanQuery.end());
@@ -54,20 +86,7 @@ std::pair<double, double> Geometry::QuadraticRoots(const std::string& query) {
         const double x2 = (-this->b - discriminantSqrt)/two_a;
         std::cout << "x1: " << x1 << " x2: " << x2 << std::endl;
         // Expressing in surd form gives:
-        auto findSimplestSurd = [&](const double& d) {
-            // First check if the number is a prime number. If so, leave it untouched
-            bool is_prime{};
-            double result = 0;
 
-            for (size_t i = 2; i <= std::sqrt(d); i++) {
-                if (std::fmod(d, i) != 0) {
-                    is_prime = true;
-                    break;
-                }
-            }
-            if (is_prime) return d;
-
-        };
         std::cout << "Expressed in surd form: " <<
             (std::fmod(this->b, two_a) == 0.0 ? -this->b/two_a : -this->b) << "+- _/"<< discriminant <<"\n";
     }
@@ -76,16 +95,17 @@ std::pair<double, double> Geometry::QuadraticRoots(const std::string& query) {
     if (discriminant < 0) {
         // Express the result in its simplest form, should b be a multiple of 2a
         if (std::fmod(this->b, two_a) == 0.0) {
-            std::cout << "x1: "<< -this->b / two_a << " + sqrt("<<discriminant<< ")" <<
-                " or x1: " << -this->b / two_a <<(discriminant > 0.0 ? "+":"-") << std::abs(discriminant) <<"i\n";
-            std::cout << "x2: "<< -this->b / two_a << " + sqrt("<<discriminant<< ")" <<
-                " or x2: " << -this->b / two_a <<(discriminant > 0.0 ? "+":"-") << std::abs(discriminant) <<"i\n";
+            // Express it as: a ± bi
+            std::cout << "x1: " << -this->b / two_a <<(discriminant > 0.0 ? "+":"-") << std::abs(discriminant) <<"i\n";
+            // If discriminant is negative, make it positive since -1*-1 = 1
+            std::cout << "x2: " << -this->b / two_a <<(discriminant < 0.0 ? "+":"-") << std::abs(discriminant) <<"i\n";
         }else {
             std::cout << "x1: " <<-this->b << "+ √"<<discriminant << ")/" << two_a<<"\n";
             std::cout << "x2: " <<-this->b << "- √"<<discriminant << ")/" << two_a<<"\n";
         }
     }
     VertexQuadraticForm();
+    CompareAndSolveEquation();
 
     return std::make_pair(1.0, 1.0);
 }
@@ -94,10 +114,34 @@ void Geometry::VertexQuadraticForm() {
     /* This is expressed in the form (x+a)2+b. It is achieved by completing the square. First, find
      * the value of a, which is b/2. Add it to ax2+bx and subtract it from c.
      */
-    const double a = this->b / 2.0;
-    const double b = -std::pow(a,2) + this->c;
+    this->a_v = this->b / 2.0;
+    this->b_v = -std::pow(a_v,2) + this->c;
 
-    std::cout << "(" << this->a << "x + " << std::abs(a) << ")2" << (b > 0.0 ? " + ":" - ") << std::abs(b);
+    std::cout << "Expressed in the form (x+a)2 + b: " <<
+        "(" << this->a << "x" << (this->a_v > 0 ? "+":"-")<< std::abs(this->a_v) << ")2"
+    << (this->b_v > 0 ? " + ":" - ") << std::abs(this->b_v) <<"\n";
+    // Ascertain whether f(x) is positive for all values of x
+    if (b_v > 0) {
+        std::cout << "f(x) > 0 for all values of x\n";
+    }
+}
+
+void Geometry::CompareAndSolveEquation(){
+    /* Here, compare f(x) to the vertex form of the equation. Normally, f(x) = 0. Therefore, simulate
+     * taking b to the other side of the equation. If it is negative, it becomes positive and vice versa.
+     */
+    if (this->b_v < 0)
+        this->b_v = std::abs(this->b_v);// make it positive
+    else
+        this->b_v = -(this->b_v);// negate it
+    /* Next, find its square root. If it is negative, display it as an imaginary number. If it is a perfect square, find
+     * the square root. If not a perfect square, display it as the simplest surd
+     */
+    if (!isPerfectSquare(this->b_v)) {
+        std::cout << this->b_v << " is not a perfect square. Invoking surd func...\n";
+    }else {
+        std::cout << this->b_v << " is a perfect square";
+    }
 }
 
 
--- src/geometry.h
+++ src/geometry.h
@@ -1,19 +1,23 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
+#include <algorithm>
 #include <regex>
 #include <string>
 #include <utility>
+#pragma once
 
 struct QuadraticPattern {
     std::regex pattern;
     std::string name;// is it linear, quadratic...etc
 };
 class Geometry {
+
 public:
     Geometry();
     ~Geometry();
     std::pair<double, double> QuadraticRoots(const std::string& query);
     void VertexQuadraticForm();
+    void CompareAndSolveEquation();
 
 private:
     double a{};
@@ -21,5 +25,6 @@ private:
     double c{};
     double inner_surd{};
     double outer_surd{};
+    double a_v{}, b_v{};
 };
 #endif //GEOMETRY_H
